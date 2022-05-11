3adc1909839f3229f8b6b15a34891d67398e30b5
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-05-11 11:29:07 +0300

    show popular posts on main page.

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -7,11 +7,11 @@ env.read_env()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS',)
 
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
